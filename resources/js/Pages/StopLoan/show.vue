e37c98743b22a804bc4f6657c0b6a1380094e31b
WebMaster-php <maanjut425@gmail.com>
2024-10-20 15:06:13 +0500

    stop_order_print

--- resources/js/Pages/StopLoan/show.vue
+++ resources/js/Pages/StopLoan/show.vue
@@ -9,81 +9,57 @@
                 Stop Order #{{ stopLoan.id }}
             </h2>
         </template>
-        <div class=" mx-auto">
-            <div class="bg-white rounded shadow p-6">
+        <div class="bg-white rounded shadow p-6">
                 <div class="text-right">
                     <button class="btn btn-primary" type="button" @click="printDiv()" id="print_btn">Print</button>
                 </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
-                    <div>
-                        <img :src="this.assetUrl + 'images/mopado-high-resolution-logo-1.png'"
-                            style="height: 200px; margin:0; padding: 0;" alt="">
-                    </div>
-                    <div>
-                        <h2>Member of Parliment And Desiginated Office Bearers Pension Fund</h2>
-                        <table>
+        </div>
+        <div class=" mx-auto main-div">
+            <div class="bg-white rounded shadow p-6">
+               
+                <div class="grid grid-cols-1 md:grid-cols-1 gap-2 mt-4">
+            
+                        <h1><strong>Member of Parliment And Desiginated Office Bearers Pension Fund</strong></h1>
+                        <table style="width: 100%;">
                             <tr>
-                                <td class="p-2 font-medium text-gray-500">
-                                    4<sup>th</sup> Floor, Liqagha House
+                                <td class="p-2 font-medium text-gray-500" style="width: 50%;">
+                                    <img :src="this.assetUrl + 'images/mopado-high-resolution-logo-1.png'" 
+                                        style="height: 200px; margin:0; padding: 0;" 
+                                        alt="">
                                 </td>
-                                <td class="p-2 font-medium text-gray-500">
-                                    P.O. Box 4639
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="p-2 font-medium text-gray-500">
-                                    Office No. 401, Manzini
-                                </td>
-                                <td class="p-2 font-medium text-gray-500">
-                                    Manzini Swaziland
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="p-2 font-medium text-gray-500">
-                                    Nkoseluhlaza Street
-                                </td>
-                                <td class="p-2 font-medium text-gray-500">
-                                    Tel:(00268) 2505 7033/4
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="p-2 font-medium text-gray-500">
-
-                                </td>
-                                <td class="p-2 font-medium text-gray-500">
-                                    Fax:(00268) 2505 7034
+                                <td class="p-2 font-medium text-gray-500" style="width: 50%;">
+                                    <table style="width: 100%;">
+                                        <tr>
+                                            <td>4<sup>th</sup> Floor, Liqagha House</td>
+                                        </tr>
+                                        <tr>
+                                            <td>P.O. Box 4639</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Office No. 401, Manzini</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Manzini Swaziland</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Nkoseluhlaza Street</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tel:(00268) 2505 7033/4</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Fax:(00268) 2505 7034</td>
+                                        </tr>
+                                    </table>
                                 </td>
                             </tr>
                         </table>
-                        <!-- <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
-                            <div>
-                                <p>4<sup>th</sup> Floor, Liqagha House</p>
-                                <p>Office No. 401, Manzini</p>
-                                <p>Nkoseluhlaza Street</p>
-                            </div>
-                            <div>
-                                <p>P.O. Box 4639</p>
-                                <p>Manzini Swaziland</p>
-                                <p>Tel:(00268) 2505 7033/4</p>
-                                <p>Fax:(00268) 2505 7034</p>
-                            </div>
-                        </div> -->
-                    </div>
+
                 </div>
                 <hr />
-                <ol class="relative border-s border-gray-200">
-                    <li class="mb-10 ms-6">
-                        <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
-                            <div>{{ stopLoan.member_account_holder }}</div>
+               
 
-                        </div>
-                    </li>
-                </ol>
-
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
-                    <div>
-
-                    </div>
+                <div class="grid grid-cols-1 md:grid-cols-1 gap-2 mt-4">
                     <div id="printDiv">
                         <table class="w-full border-collapse border border-gray-200">
                             <tr class="text-left bg-slate-50">
@@ -104,10 +80,23 @@
                                     }}</td>
                             </tr>
                             <tr class="text-left bg-slate-50">
+                                <th class="border border-gray-200 p-2 font-medium text-gray-500">Member Account Holder Name
+                                </th>
+                                <td class="border border-gray-200 p-2 font-medium text-gray-500">
+                                        {{ stopLoan.member_account_holder }}</td>
+                            </tr>
+                            <tr class="text-left bg-slate-50">
                                 <th class="border border-gray-200 p-2 font-medium text-gray-500">Member Account Number
                                 </th>
                                 <td class="border border-gray-200 p-2 font-medium text-gray-500">{{
                                     stopLoan.member_account_number }}</td>
+                            </tr>
+                       
+                            <tr class="text-left bg-slate-50">
+                                <th class="border border-gray-200 p-2 font-medium text-gray-500">Member Branch Code
+                                </th>
+                                <td class="border border-gray-200 p-2 font-medium text-gray-500">
+                                    {{stopLoan.member_branch_code }}</td>
                             </tr>
                             <tr class="text-left bg-slate-50">
                                 <th class="border border-gray-200 p-2 font-medium text-gray-500">Monthly Installment
@@ -132,30 +121,35 @@
                                 <td class="border border-gray-200 p-2 font-medium text-gray-500"></td>
                             </tr> -->
                         </table>
+                        <table>
+                            <tr>
+                                <td colspan="3">
+                                    <p class="mt-5">I understand that this stop order instructions shall remain in force until advised
+                                        otherwise by
+                                        MOPADO only.</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><div class="child-div">_____________________<br>Account Holder</div></td>
+                                <td><div class="child-div">_____________________<br>MOPADO</div></td>
+                                <td><div style="border: 1px solid black; height: 100px; width: 150px; text-align: center; margin: 0;">Stamp
+                                </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                  
+                                </td>
+                                <td colspan="2">
+                                    <div style="border: 1px solid black; height: 100px; width: 150px; text-align: center;">BANK
+                                        STAMP</div>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
-                </div>
-                <p class="mt-5">I understand that this stop order instructions shall remain in force until advised
-                    otherwise by
-                    MOPADO only.</p>
-                <div class="main-div" style="">
-                    <div class="child-div">_____________________<br>Account Holder</div>
-                    <div class="child-div">_____________________<br>MOPADO</div>
-                    <div class="child-div">
-                        <div style="border: 1px solid black; height: 100px; width: 150px; text-align: center; margin: 0;">Stamp
-                        </div>
-                    </div>
-                </div>
-                <div class="main-div">
-                    <div class="child-div"></div>
-                    <div class="child-div">
-                        <div style="border: 1px solid black; height: 100px; width: 150px; text-align: center;">BANK
-                            STAMP</div>
-                    </div>
-                    <div class="child-div"></div>
-                </div>
+            </div>
             </div>
         </div>
-
         <teleport to="head">
             <title>{{ pageTitle }}</title>
             <meta property="og:description" :content="pageDescription">
@@ -358,52 +352,71 @@ export default {
 <style scoped>
 @media print {
     body {
-        visibility: hidden;
+        visibility: hidden; /* Hide entire body */
+        margin: 0; /* Remove margins */
     }
 
     #print_btn {
-        visibility: hidden;
+        visibility: hidden; /* Hide the print button */
     }
 
-    /* #printDiv {
-    visibility: visible;
-    position: absolute;
-    left: 0;
-    top: 0;
-  } */
+    .main-div {
+        visibility: visible; /* Make main content visible */
+        position: absolute; /* Positioning to fit on page */
+        left: 0; /* Align to left */
+        top: 0; /* Align to top */
+        margin: 0; /* Ensure no margins */
+        padding: 0; /* Ensure no padding */
+        width: 100%; /* Full width */
+    }
+
+    .child-div {
+        display: inline-block; /* Maintain inline-block layout */
+        width: 30%; /* Set width of each child */
+        margin: 0; /* No margins */
+        padding: 0; /* No padding */
+        box-sizing: border-box; /* Include padding and borders in width */
+    }
+
+    /* Optional: Adjust text size for print if needed */
+    body * {
+        visibility: hidden; /* Hide all elements */
+    }
+
+    .main-div, .main-div * {
+        visibility: visible; /* Show main content and its children */
+    }
+
+    /* Additional print-specific styling */
+    .main-div {
+        page-break-inside: avoid; /* Avoid page breaks inside this div */
+    }
+
+    /* Ensure child divs stack neatly */
+    .child-div {
+        width: auto; /* Allow child divs to fit content */
+        text-align: left; /* Align text to the left for better print readability */
+    }
 }
 
+/* General styles */
 .main-div {
-    width: 100%;
-    /* Full width of parent */
-    padding: 10px;
-    /* border: 1px solid #000; */
-    /* Optional: border for visualization */
-    box-sizing: border-box;
-    /* Include padding in width calculation */
+    width: 100%; /* Full width of parent */
 }
 
 .child-div {
-    display: inline-block;
-    /* Use inline-block for horizontal layout */
-    width: 30%;
-    /* Set width of each child div */
-    margin: 0 1%;
-    /* Space between divs */
-    padding: 20px;
-    /* background-color: #f0f0f0; */
-    /* Light gray background */
-    text-align: center;
-    box-sizing: border-box;
-    /* Include padding in width calculation */
+    display: inline-block; /* Use inline-block for horizontal layout */
+    width: 30%; /* Set width of each child div */
+    margin: 0; /* No extra space */
+    text-align: center; /* Center align text */
 }
 
+/* Responsive adjustments */
 @media (max-width: 600px) {
     .child-div {
-        width: 100%;
-        /* Stack divs on smaller screens */
-        margin: 10px 0;
-        /* Vertical space for stacked divs */
+        width: 100%; /* Stack divs on smaller screens */
+        margin: 10px 0; /* Vertical space for stacked divs */
     }
 }
+
 </style>
